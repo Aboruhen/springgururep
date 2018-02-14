7bfc8c3ecdc148d713e90344a739bfb99aea5c1b
RomanKniazevych <aboruhen@gmail.com>
2018-02-14 15:22:08 +0200

    add models

---
+++ src/main/java/guru/springframework/kniaz/springkniaz/SpringkniazApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.kniaz.springkniaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringkniazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringkniazApplication.class, args);
+	}
+}
---
+++ src/test/java/guru/springframework/kniaz/springkniaz/SpringkniazApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.kniaz.springkniaz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringkniazApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
