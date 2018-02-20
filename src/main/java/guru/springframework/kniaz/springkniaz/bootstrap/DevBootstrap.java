d0c69c7cb717a9cf105f0ff23a0517688438121c
RomanKniazevych <aboruhen@gmail.com>
2018-02-20 17:13:14 +0200

    add author controller

---
+++ src/main/java/guru/springframework/kniaz/springkniaz/bootstrap/DevBootstrap.java
@@ -0,0 +1,52 @@
+package guru.springframework.kniaz.springkniaz.bootstrap;
+
+import guru.springframework.kniaz.springkniaz.model.Author;
+import guru.springframework.kniaz.springkniaz.model.Book;
+import guru.springframework.kniaz.springkniaz.model.Publisher;
+import guru.springframework.kniaz.springkniaz.repositories.AuthorRepository;
+import guru.springframework.kniaz.springkniaz.repositories.BookRepository;
+import guru.springframework.kniaz.springkniaz.repositories.PublisherRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        Publisher publisher = new Publisher();
+        publisher.setName("foo");
+
+        publisherRepository.save(publisher);
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "1234", publisher);
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+
+        //Rod
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", publisher);
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+    }
+
+}
---
+++ src/main/java/guru/springframework/kniaz/springkniaz/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.kniaz.springkniaz.repositories;
+
+import guru.springframework.kniaz.springkniaz.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long>{
+}
